d83a0f7476b937a983838a3c84462b47b33154a1
Manish Raj Silwal <manishraj.silwal@gmail.com>
2019-08-27 09:54:00 +0545

    Static pages added

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -5,59 +5,20 @@
     <div class="row mb-4">
 
 
-      <div class="col-md-4 col-12 pt-4 d-flex align-content-center flex-wrap">
-        <!-- <div class="col-12 align-self-center">
-          <div class="card login-background-card">
-            <p>Admin </p>
-            <p>Panel</p> -->
-            <div class="card shadow text-center login-card">
-              <!-- <b-img src="/dental.png" fluid height="100" width="100" class="align-self-center mb-4"></b-img> -->
-              <p class="title-text pb-4">
-                Dental Hub
-              </p>
-              <b-form @submit.prevent>
-                <b-form-group class="pb-3" label-for="email">
-                  <b-form-input id="email" type="email" v-model="username" placeholder="Email Address"></b-form-input>
-                </b-form-group>
-
-                <b-form-group class="pb-3" label-for="password">
-                  <b-form-input id="password" type="password" v-model="password" placeholder="Password"></b-form-input>
-                </b-form-group>
-
-  <!--               <b-form-group class="pb-3" label-for="rememberme">
-                  <b-form-checkbox-group>
-                    <b-form-checkbox value="">Remember Me</b-form-checkbox>
-                  </b-form-checkbox-group>
-                </b-form-group>
-   -->
-                <div class="col-md-12 submit-button text-center">
-                  <b-button block variant="custom" type="submit" @click="processLogin">Sign in</b-button>
-                </div>
-              </b-form>
-              <div class="row mt-4 justify-content-center">
-                <b-nav pills>
-                  <b-nav-item>About</b-nav-item>
-                  <b-nav-item>Help</b-nav-item>
-                  <b-nav-item>Privacy Policy</b-nav-item>
-                </b-nav>
-              </div>
-              <small class="mt-4">&copy; 2019 | Designed, Developed & Maintained by <a href="https://abhiyantrik.com">Abhiyantrik Technology</a></small>
-            </div>
-          <!-- </div>
-        </div> -->
+      <div class="col-md-4 col-12 d-flex align-content-center flex-wrap">
+        <LoginForm></LoginForm>
       </div>
 
 
       <div class="col-md-8 col-12">
         <div class="row mt-4">
-          <div class="col-md-4 col-12">
+          <div class="col-md-4 col-12" data-aos="fade-down" data-aos-duration="3200">
             <div class="card shadow">
               <p class="counter">
                 <ICountUp
                 :delay="delay"
-                :endVal="endVal[0]"
+                :endVal="treatment_obj"
                 :options="options"
-                @ready="onReady"
                 />
               </p>
               <small class="counter-label">
@@ -66,14 +27,13 @@
             </div>
           </div>
 
-          <div class="col-md-4 col-12">
+          <div class="col-md-4 col-12" data-aos="fade-down" data-aos-duration="3200">
             <div class="card shadow">
               <p class="counter">
                 <ICountUp
                 :delay="delay"
-                :endVal="endVal[1]"
+                :endVal="cavities_obj"
                 :options="options"
-                @ready="onReady"
                 />
               </p>
               <small class="counter-label">
@@ -82,14 +42,13 @@
             </div>
           </div>
 
-          <div class="col-md-4 col-12">
+          <div class="col-md-4 col-12" data-aos="fade-down" data-aos-duration="3200">
             <div class="card shadow">
               <p class="counter">
                 <ICountUp
                 :delay="delay"
-                :endVal="endVal[2]"
+                :endVal="contacted_obj"
                 :options="options"
-                @ready="onReady"
                 />
               </p>
               <small class="counter-label">
@@ -101,7 +60,7 @@
 
         <div class="row mt-4">
           <div class="col-12 text-center">
-            <div class="card shadow">
+            <div class="card shadow" data-aos="fade-left" data-aos-duration="3200">
               <p class="counter chart-counter">
                 <LoginVisualization :tag="lch" :type="type" :clean-data="locationChart"></LoginVisualization>
               </p>
@@ -114,52 +73,33 @@
         </div>
 
       </div>
-
-    <!-- </div> -->
-  </div>
-
-    <b-toast id="error-toast" variant="danger" solid append-toast toaster="b-toaster-bottom-full">
-      <div slot="toast-title" class="d-flex flex-grow-1 align-items-baseline">
-        <strong class="mr-auto">Login Error</strong>
-      </div>
-      <div v-if="errors.auth">
-        <p>{{ errors.auth }}</p>
-      </div>
-      <div v-if="require_error.username">
-        <p>{{ require_error.username }}</p>
-      </div>
-      <div v-if="require_error.password">
-        <p>{{ require_error.password }}</p>
-      </div>
-    </b-toast>
+    </div>
   </div>
 </div>
 </template>
 
 <script>
 import { mapState,mapMutations } from 'vuex';
+
+import LoginForm from './LoginForm';
 import LoginVisualization from './LoginPageVisualization';
 import locationChart from '../js/locationchart.js';
 
 const axios = require('axios');
+
 export default {
   name: "LoginPage",
   components:{
+    'LoginForm': LoginForm,
     'LoginVisualization': LoginVisualization
   },
-  computed: {
-    ...mapState([
-      'token','adminusername'
-    ])
-  },
+
   data() {
     return {
-      username: '',
-      password: '',
-      errors: {'auth':''},
-      require_error:[],
       delay: 100,
-      endVal: '',
+      treatment_obj:500,
+      cavities_obj:1500,
+      contacted_obj:6000,
       endVal: [500, 1500, 6000],
       options: {
         useEasing: true,
@@ -174,66 +114,6 @@ export default {
       lch:"lch",
       type: "pie",
     }
-  },
-
-  created(){
-    var token = window.localStorage.getItem("token");
-    if (token != undefined && token != ""){
-      this.$router.replace("/dashboard");
-    }
-  },
-
-  methods:{
-    ...mapMutations([
-      'setToken','setAdminUsername'
-    ]),
-
-    onReady: function(instance, CountUp) {
-      const that = this;
-      instance.update(that.arr(endVal));
-    },
-
-    processLogin(){
-      this.require_error=[]
-      if(this.$data.username==''){
-        this.require_error['username']="Email required."
-        this.$bvToast.show('error-toast');
-
-      }
-      else if(this.$data.password==''){
-        this.require_error['password']="Password required."
-        this.$bvToast.show('error-toast');
-      }
-      else{
-        axios.post('http://104.155.163.124:3000/api/v1/checkuser',{'email':this.$data.username}).then(response=>{
-          console.log(response.data)
-          this.setAdminUsername(response.data.username)
-          var formData = {'username': this.adminusername, 'password': this.$data.password}
-          axios.post('http://104.155.163.124:3000/api/v1/token/obtain',formData)
-          .then(response => {
-            window.localStorage.setItem("token", response.data.token);
-            this.setToken(response.data.token);
-            axios.defaults.headers.common['authorization']  = 'JWT ' + response.data.token
-            this.$router.replace("/dashboard");
-          })
-          .catch(error => {
-            if(error){
-              this.errors['auth'] = "Invalid username/password."
-              this.$bvToast.show('error-toast');
-            }
-            this.errored = true;
-          })
-        })
-        .catch(error => {
-          if(error){
-            this.errors['auth'] =  "Invalid username/password."
-            this.$bvToast.show('error-toast');
-          }this.errored = true;
-
-        })
-      }
-    }
-
   }
 };
 </script>
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,16 @@ import IndexPage from '../components/IndexPage.vue';
 import Settings from '../components/Settings.vue';
 import LoginPage from '../components/LoginPage.vue';
 import Logout from '../components/Logout.vue';
+import About from '../components/AboutPage.vue';
+import Help from '../components/HelpPage.vue';
+import PrivacyPolicy from '../components/PrivacyPolicyPage.vue';
 
 export default[
   {path: '/dashboard', name: 'index', component: IndexPage},
   {path: '/settings', name: 'settings', component: Settings},
   {path: '/', name: 'loginpage', component: LoginPage},
   {path: '/logout', name: 'logout', component: Logout},
+  {path: '/about', name: 'about', component: About},
+  {path: '/help', name: 'help', component: Help},
+  {path: '/privacypolicy', name: 'privacypolicy', component: PrivacyPolicy},
 ]
